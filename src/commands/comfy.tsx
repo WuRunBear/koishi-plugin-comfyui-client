41339180c0cb849a08e06c7fb6bb420ea2f3d456
Alec <wei-yujie@qq.com>
2026-09-04 16:49:24 +0800

    fix: support image in same message and strip img tags from prompt

--- src/commands/comfy.tsx
+++ src/commands/comfy.tsx
@@ -65,8 +65,9 @@ export function registerComfyCommand(ctx: Context) {
 
       const message = _.session.event.message
       let imgQu: any[] = []
+      imgQu = h.select(message.elements || [], 'img')
       if (message.quote) {
-        imgQu = h.select(message.quote.elements, 'img')
+        imgQu = imgQu.concat(h.select(message.quote.elements, 'img'))
       }
 
       // 交互式上传模式
@@ -163,8 +164,11 @@ export function registerComfyCommand(ctx: Context) {
         return
       }
 
-      if (message.quote && userPrompt) {
-        userPrompt = userPrompt.replaceAll(h.unescape(message.quote.content), ' ')
+      if (userPrompt) {
+        userPrompt = userPrompt
+          .replace(/<img[^>]*>/g, ' ')
+          .replace(/\[图\d*\]/g, ' ')
+          .replaceAll(h.unescape(message.quote ? message.quote.content : ''), ' ')
       }
 
       // 支持多图：构建 images 列表
